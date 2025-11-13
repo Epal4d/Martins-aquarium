ddf418017b5e16e1cc435bf6db59a20b17a9b192
Evan Alford <evan.al4d@gmail.com>
2025-11-13 15:48:05 -0600

    initial commit

---
+++ scripts/aquariumData.js
@@ -0,0 +1,60 @@
+export const database = {
+    fish: [
+        {
+            name: "Nemo",
+            species: "Clownfish",
+            length: 3,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://example.com/images/nemo.jpg"
+        },
+        {
+            name: "Dory",
+            species: "Blue Tang",
+            length: 5,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://example.com/images/dory.jpg"
+        },
+        {
+            name: "Regal",
+            species: "Emperor Angelfish",
+            length: 12,
+            location: "Indo-Pacific Coral Reefs",
+            diet: "Sponges & small invertebrates",
+            image: "https://upload.wikimedia.org/wikipedia/commons/1/18/Emperor_angelfish_adult.jpg"
+        },
+        {
+            name: "Gill",
+            species: "Moorish Idol",
+            length: 8,
+            location: "Indo-Pacific Reefs",
+            diet: "Sponges, algae, coral polyps",
+            image: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Moorish_Idol_Zanclus_cornutus.jpg"
+        },
+        {
+            name: "Spot",
+            species: "Blackspot Sergeant",
+            length: 7,
+            location: "Tropical tidepools & reefs",
+            diet: "Algae & small plankton",
+            image: "https://upload.wikimedia.org/wikipedia/commons/6/6b/Abudefduf_sordidus.jpg"
+        },
+        {
+            name: "Banner",
+            species: "Schooling Bannerfish",
+            length: 10,
+            location: "Indo-Pacific Reefs",
+            diet: "Zooplankton",
+            image: "https://upload.wikimedia.org/wikipedia/commons/3/3c/Heniochus_diphreutes.JPG"
+        },
+        {
+            name: "Wrassy",
+            species: "Yellowtail Coris Wrasse",
+            length: 12,
+            location: "Indo-Pacific Reef Slopes",
+            diet: "Small crustaceans & worms",
+            image: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Coris_gaimard_juvenile.jpg"
+        }
+    ]
+}
---
+++ scripts/fishList.js
@@ -0,0 +1,19 @@
+import { database } from './aquariumData.js';
+
+const fish = database.fish
+
+export const fishList = () => {
+    // Generate an HTML representation of each fish
+    let fishHTML = '';
+        for (const fishies of fish) {
+            fishHTML += `
+            <article class='fish'>
+                <img src="${fishies.image}" alt="${fishies.name} poster" class= "fish__image">
+                <div class="fish__list" ${fishies.name} </div>
+            </article>
+        `
+    }
+    return fishHTML
+
+        
+}
