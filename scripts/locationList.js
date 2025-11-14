dc49c13065ba2c10f5fb586519ad7603cec05182
Evan Alford <evan.al4d@gmail.com>
2025-11-14 12:18:58 -0600

    functionality added to location list module

--- scripts/locationList.js
+++ scripts/locationList.js
@@ -1,14 +1,18 @@
 import { database } from "./aquariumData.js";
 
-// const locations = database.locations
+const locations = database.locations
 
-// const locationList = () => {
-//     let locationListHTML = '<article class="locations"'
-//         for (const location of locations) {
-//             locationListHTML += `<section class="location">
-//                 Name:${location.name}<br>
-//                 Country: ${location.countryr}    
-//             </section>`
+const locationList = () => {
+    let locationListHTML = '<article class="locations"'
+        for (const location of locations) {
+            locationListHTML += `<section class="location">
+                Name:${location.name}<br>
+                Country: ${location.country}<br>
+                Description: ${location.description}<br>    
+            </section>
+        `
             
-//         }
-// }
+    }
+        locationListHTML += "</article>"
+    return locationListHTML 
+}
